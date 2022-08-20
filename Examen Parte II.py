9c66025037b821ce7050a1e12c3b55c6879ba381
N1ckol <mnbarrientos@unah.hn>
2022-08-19 21:10:34 -0600

    inicio de interfaz

---
+++ Examen Parte II.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 19:21:50 2022
+
+@author: Dell
+"""
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+import os
+from tkinter.messagebox import showinfo
+
+#Establecer ventana
+vent = tk.Tk()
+vent.geometry('1400x700')
+
+vent.title('Calculo de Zonas Inundadas')    #Titulo
+vent.config(bg= 'turquoise1')
+
