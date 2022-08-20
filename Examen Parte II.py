89238883c4dfa735fb0e2437c887c46fd5d7d244
N1ckol <mnbarrientos@unah.hn>
2022-08-19 21:33:23 -0600

    cajas

--- Examen Parte II.py
+++ Examen Parte II.py
@@ -58,3 +58,26 @@ def abrir_shape():
 
 boton2 = tk.Button(text="Seleccione el shapefile ", font = 'Arial 10', bg="white", command = abrir_shape)
 boton2.grid(row = 4, column = 1)
+# caja 3 #
+
+
+etiq3 = tk.Label(vent, text='3. Proceda a pre-procesar la imagen.', font = 'Arial 10', bg='gray85',  fg='black')
+etiq3.grid(row = 6, column = 1)
+
+
+
+boton3 = tk.Button(text="Preprocesar la imagen", font = 'Arial 10', bg="white")
+boton3.grid(row = 7, column = 1)
+
+# Caja 4 #
+
+
+etiq4 = tk.Label(vent, text='4. Defina el umbral de la mascara de agua.', font = 'Arial 10', bg='gray85',  fg='black')
+etiq4.grid(row = 9, column = 1)
+
+
+umbral = tk.Entry(vent, font= 'Arial 10', justify = 'center', highlightthickness = 3)
+umbral.grid(row = 10, column = 1, pady = (3))
+
+boton4 = tk.Button(text="Aplicar la mascara", font = 'Arial 10', bg="white")
+boton4.grid(row = 11, column = 1, pady =(3))
